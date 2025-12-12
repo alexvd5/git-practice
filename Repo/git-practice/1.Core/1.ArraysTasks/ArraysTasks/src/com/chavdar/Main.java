475424a8ac0ec79479d1c67cbfa369f9e1698606
a_yordanovv <106471271+alexvd5@users.noreply.github.com>
2025-12-12 19:48:58 +0200

    Update Main.java

--- Repo/git-practice/1.Core/1.ArraysTasks/ArraysTasks/src/com/chavdar/Main.java
+++ Repo/git-practice/1.Core/1.ArraysTasks/ArraysTasks/src/com/chavdar/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
 
-        System.out.println("Now I can modify the remote copy of the repository.");
+        System.out.println("New conflict");
     }
 }
