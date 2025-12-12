b94a72ffa0f7d6e17bbd725b2dc85529158d3084
a_yordanovv <aleksandar_yordanov2001@abv.bg>
2025-12-12 15:32:10 +0200

    Update main method

--- Repo/git-practice/1.Core/1.ArraysTasks/ArraysTasks/src/com/chavdar/Main.java
+++ Repo/git-practice/1.Core/1.ArraysTasks/ArraysTasks/src/com/chavdar/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
 
-        System.out.println("Now I can continue make changes to my)");
+        System.out.println("Now I can cause merge conflict with the remote copy.");
     }
 }
